993ef7c44fa2d2c9d5a1d8f91d53bd50efb0acfd
Louis-Maël Derval <louismael40@gmail.com>
2022-01-26 23:18:32 +0100

    TP8 Physique

--- pcsi3/physique/TP8.py
+++ pcsi3/physique/TP8.py
@@ -1,12 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+T = np.array([1, 2, 3, 4, 5])
+u_T = 1
+
+t = np.array([1, 1, 1, 1, 1])
+u_t = 1
+
 # Abscisse
-x = np.array([1, 2, 3, 4, 5])
-u_x = np.array([0.1, 0.1, 0.2, 0.1, 0.1])
+x = np.log(T / t)
+u_x = np.log(u_T / u_t)
 
 # Ordonnée
 y = np.array([3, 5, 7, 9, 11])
+y = x / 2
 u_y = 0.5
 
 # Calcul de la valeur centrale de la mesure par régression linéaire y = ax+b
@@ -35,17 +42,17 @@ u_b = np.std(liste_b, ddof=1)
 xfit = np.linspace(min(x), max(x), 2)  # 2 points suffisent pour tracer la droite
 yfit = a * xfit + b
 plt.figure()
-plt.plot(xfit, yfit, 'r', label="a = {:.2e} +/- {:.2e} unité, b = {:.2e} +/- {:.2e} unité ".format(a, u_a, b, u_b))
+plt.plot(xfit, yfit, 'r', label="a = {:.2e} +/- {:.2e} rad-1, b = {:.2e} +/- {:.2e}".format(a, u_a, b, u_b))
 plt.errorbar(x, y, xerr=u_x, yerr=u_y, fmt='b+', zorder=2, label='Mesures')
-plt.title("Régression linéaire y = ax+b")
-plt.xlabel("x (unité)")
-plt.ylabel("y (unité)")
+plt.title(r"Régression linéaire $\ln \frac{T}{t} = ax+b$")
+plt.xlabel(r"$\alpha$ en $rad^{-1}$")
+plt.ylabel(r"$\ln \frac{T}{t}$")
 plt.legend()
 plt.show()
 
 # Résultat de la régression linéaire
-print("pente = {:.2e} +/- {:.2e} unité".format(a, u_a))
-print("ordonnée = {:.2e} +/- {:.2e} unité".format(b, u_b))
+print("pente = {:.2e} +/- {:.2e} rad-1".format(a, u_a))
+print("ordonnée = {:.2e} +/- {:.2e}".format(b, u_b))
 
 # Tracé des résidus
 res = y - (a * x + b)
@@ -53,8 +60,8 @@ plt.figure()
 plt.plot(xfit, [0, 0], 'r')
 plt.errorbar(x, res, xerr=None, yerr=u_y, fmt='b+', zorder=2, label='Résidus')
 plt.title("Résidus (écarts verticaux à la droite de régression)")
-plt.xlabel("x")
-plt.ylabel("y")
+plt.xlabel(r"$\alpha$ en $rad^{-1}$")
+plt.ylabel(r"$\ln \frac{T}{t}$")
 plt.legend()
 plt.show()
 
@@ -65,7 +72,6 @@ plt.plot(xfit, [2, 2], 'r')
 plt.plot(xfit, [-2, -2], 'r')
 plt.scatter(x, ecart)
 plt.title("Ecarts normalisés")
-plt.xlabel("x")
+plt.xlabel(r"$\alpha$ en $rad^{-1}$")
 plt.ylabel("écart norm.")
-plt.legend()
 plt.show()
